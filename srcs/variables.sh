ec87476433fb190bb41a7694767cfbfca9f4abc5
Julien TOTY <jtoty@e1r4p11.42.fr>
2016-11-03 18:45:45 +0100

    Separation en deux parties (en cours)

--- srcs/variables.sh
+++ srcs/variables.sh
@@ -18,6 +18,12 @@ Part1=('ft_memset.c' 'ft_bzero.c' 'ft_memcpy.c' 'ft_memccpy.c' 'ft_memmove.c' \
 'ft_atoi.c' 'ft_isalpha.c' 'ft_isdigit.c' 'ft_isalnum.c' 'ft_isascii.c' \
 'ft_isprint.c' 'ft_toupper.c' 'ft_tolower.c')
 
+Part1_func_forbidden=('6' '5' '6' '5' '7' \
+'7' '7' '6' '6' '7' \
+'7' '6' '6' '6' '6' \
+'6' '3' '3' '3' '3' \
+'3' '3' '3' '3')
+
 Part2=('ft_memalloc.c' 'ft_memdel.c' 'ft_strnew.c' 'ft_strdel.c' 'ft_strclr.c' \
 'ft_striter.c' 'ft_striteri.c' 'ft_strmap.c' 'ft_strmapi.c' 'ft_strequ.c' \
 'ft_strnequ.c' 'ft_strsub.c' 'ft_strjoin.c' 'ft_strtrim.c' 'ft_strsplit.c' \
