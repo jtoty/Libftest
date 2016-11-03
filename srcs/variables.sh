66915c93c262cc536ab72c762d2e76d41f970d42
Julien TOTY <jtoty@e1r4p11.42.fr>
2016-11-03 18:17:40 +0100

    Separation en deux parties (en cours)

--- srcs/variables.sh
+++ srcs/variables.sh
@@ -4,6 +4,19 @@
 #               Mandatory functions               #
 ###################################################
 
+Part1=('ft_memset.c' 'ft_bzero.c' 'ft_memcpy.c' 'ft_memccpy.c' 'ft_memmove.c' \
+'ft_memchr.c' 'ft_memcmp.c' 'ft_strlen.c' 'ft_strdup.c' 'ft_strcpy.c' \
+'ft_strncpy.c' 'ft_strcat.c' 'ft_strncat.c' 'ft_strlcat.c' 'ft_strchr.c' \
+'ft_strrchr.c' 'ft_strstr.c' 'ft_strnstr.c' 'ft_strcmp.c' 'ft_strncmp.c' \
+'ft_atoi.c' 'ft_isalpha.c' 'ft_isdigit.c' 'ft_isalnum.c' 'ft_isascii.c' \
+'ft_isprint.c' 'ft_toupper.c' 'ft_tolower.c')
+
+Part2=('ft_memalloc.c' 'ft_memdel.c' 'ft_strnew.c' 'ft_strdel.c' 'ft_strclr.c' \
+'ft_striter.c' 'ft_striteri.c' 'ft_strmap.c' 'ft_strmapi.c' 'ft_strequ.c' \
+'ft_strnequ.c' 'ft_strsub.c' 'ft_strjoin.c' 'ft_strtrim.c' 'ft_strsplit.c' \
+'ft_itoa.c' 'ft_putchar.c' 'ft_putstr.c' 'ft_putendl.c' 'ft_putnbr.c' \
+'ft_putchar_fd.c' 'ft_putstr_fd.c' 'ft_putendl_fd.c' 'ft_putnbr_fd.c')
+
 mandatory_func_forbidden=('7' '7' '7' '7' '7' \
 '7' '7' '6' '6' '7' '7' \
 '7' '7' '5' '4' '7' '3' \
