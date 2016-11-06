4285b103f67db7c6a632109e59bc82a409641af1
Julien TOTY <totyj@free.fr>
2016-11-07 00:04:36 +0100

    Add test

--- tests/Part2_functions/ft_memalloc/main.c
+++ tests/Part2_functions/ft_memalloc/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "../../../libft.h"
+#include <limits.h>
 
 int		main(int argc, const char *argv[])
 {
@@ -13,7 +14,10 @@ int		main(int argc, const char *argv[])
 	{
 		str = ft_memalloc(9);
 		if (!str)
+		{
+			write(1, "NULL", 4);
 			return (0);
+		}
 		i = 0;
 		while (i < 9)
 		{
@@ -22,6 +26,21 @@ int		main(int argc, const char *argv[])
 		}
 	}
 	else if (atoi(argv[1]) == 2)
+	{
+		str = ft_memalloc(ULONG_MAX);
+		if (!str)
+		{
+			write(1, "NULL", 4);
+			return (0);
+		}
+		i = 0;
+		while (i < 9)
+		{
+			write(1, &str[i], 1);
+			i++;
+		}
+	}
+	else if (atoi(argv[1]) == 3)
 	{
 		str = ft_memalloc(0);
 		if (!str)
