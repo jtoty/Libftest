5b6b110e08eb01f5ef46760d2b5d69f793f8c541
Julien TOTY <jtoty@e1r6p9.42.fr>
2016-11-07 14:29:00 +0100

    remove split

--- srcs/variables.sh
+++ srcs/variables.sh
@@ -31,21 +31,24 @@ Part1_func=('ft_memset.c' 'ft_bzero.c' 'ft_memcpy.c' 'ft_memccpy.c' 'ft_memmove.
 
 Part2_func_forbidden=('6' '5' '6' '5' '7' \
 '7' '7' '6' '6' '7' \
-'7' '6' '6' '6' '4' \
+'7' '6' '6' '6'  \
 '6' '3' '3' '3' '3' \
 '3' '3' '3' '3')
+#'7' '6' '6' '6' '4' \
 
 Part2_func_activation=('0' '0' '0' '0' '0' \
 '0' '0' '0' '0' '0' \
-'0' '0' '0' '0' '0' \
+'0' '0' '0' '0' \
 '0' '0' '0' '0' '0' \
 '0' '0' '0' '0')
 
+#'0' '0' '0' '0' '0' \
 Part2_func=('ft_memalloc.c' 'ft_memdel.c' 'ft_strnew.c' 'ft_strdel.c' 'ft_strclr.c' \
 'ft_striter.c' 'ft_striteri.c' 'ft_strmap.c' 'ft_strmapi.c' 'ft_strequ.c' \
-'ft_strnequ.c' 'ft_strsub.c' 'ft_strjoin.c' 'ft_strtrim.c' 'ft_strsplit.c' \
+'ft_strnequ.c' 'ft_strsub.c' 'ft_strjoin.c' 'ft_strtrim.c'  \
 'ft_itoa.c' 'ft_putchar.c' 'ft_putstr.c' 'ft_putendl.c' 'ft_putnbr.c' \
 'ft_putchar_fd.c' 'ft_putstr_fd.c' 'ft_putendl_fd.c' 'ft_putnbr_fd.c')
+#'ft_strnequ.c' 'ft_strsub.c' 'ft_strjoin.c' 'ft_strtrim.c' 'ft_strsplit.c' \
 
 ###################################################
 #                 Bonus functions                 #
