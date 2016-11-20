5780b43ffe6126fe77aa5342bf9b2146da19585f
jtoty <jtoty@student.42.fr>
2016-11-20 17:40:46 +0100

    Change display output

--- tests/Additional_functions/ft_strlcpy/main.c
+++ tests/Additional_functions/ft_strlcpy/main.c
@@ -36,26 +36,31 @@ int		main(int argc, const char *argv[])
 	if (atoi(argv[1]) == 1)
 	{
 		ft_print_result(ft_strlcpy(dest, "lorem", 15));
+		write(1, "\n", 1);
 		ft_print_result2(dest, 15);
 	}
 	else if (atoi(argv[1]) == 2)
 	{
 		ft_print_result(ft_strlcpy(dest, "", 15));
+		write(1, "\n", 1);
 		ft_print_result2(dest, 15);
 	}
 	else if (atoi(argv[1]) == 3)
 	{
 		ft_print_result(ft_strlcpy(dest, "lorem ipsum", 3));
+		write(1, "\n", 1);
 		ft_print_result2(dest, 15);
 	}
 	else if (atoi(argv[1]) == 4)
 	{
 		ft_print_result(ft_strlcpy(dest, "lorem ipsum dolor sit amet", 15));
+		write(1, "\n", 1);
 		ft_print_result2(dest, 15);
 	}
 	else if (atoi(argv[1]) == 5)
 	{
 		ft_print_result(ft_strlcpy(dest, "lorem ipsum dolor sit amet", 0));
+		write(1, "\n", 1);
 		ft_print_result2(dest, 15);
 	}
 	return (0);
