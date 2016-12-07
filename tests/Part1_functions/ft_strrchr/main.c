3b4aaf9551bc0ab2a54198ca4819449f5a8715d8
jtoty <jtoty@student.42.fr>
2016-12-07 14:52:19 +0100

    Change tests ft_strrchr

--- tests/Part1_functions/ft_strrchr/main.c
+++ tests/Part1_functions/ft_strrchr/main.c
@@ -14,51 +14,73 @@ void	ft_print_result(char const *s)
 
 int		main(int argc, const char *argv[])
 {
-	const char *str;
+	char		*str;
+	char		str2[] = "bonjour";
 
 	if (argc == 1)
 		return (0);
 	if (atoi(argv[1]) == 1)
 	{
-		if (!(str = ft_strrchr("bonjour", 'b')))
+		if (!(str = ft_strrchr(str2, 'b')))
 			ft_print_result("NULL");
 		else
+		{
 			ft_print_result(str);
+			if (str != str2)
+				ft_print_result("\nReturn value is false");
+		}
 	}
 	else if (atoi(argv[1]) == 2)
 	{
-		if (!(str = ft_strrchr("bonjour", 'o')))
+		if (!(str = ft_strrchr(str2, 'o')))
 			ft_print_result("NULL");
 		else
+		{
 			ft_print_result(str);
+			if (str != str2 + 4)
+				ft_print_result("\nReturn value is false");
+		}
 	}
 	else if (atoi(argv[1]) == 3)
 	{
-		if (!(str = ft_strrchr("bonjourno", 'o')))
+		char	str3[]= "bonjourno";
+		if (!(str = ft_strrchr(str3, 'o')))
 			ft_print_result("NULL");
 		else
+		{
 			ft_print_result(str);
+			if (str != str3 + 8)
+				ft_print_result("\nReturn value is false");
+		}
 	}
 	else if (atoi(argv[1]) == 4)
 	{
-		if (!(str = ft_strrchr("bonjour", 'j')))
+		if (!(str = ft_strrchr(str2, 'j')))
 			ft_print_result("NULL");
 		else
+		{
 			ft_print_result(str);
+			if (str != str2 + 3)
+				ft_print_result("\nReturn value is false");
+		}
 	}
 	else if (atoi(argv[1]) == 5)
 	{
-		if (!(str = ft_strrchr("bonjour", 's')))
+		if (!(str = ft_strrchr(str2, 's')))
 			ft_print_result("NULL");
 		else
 			ft_print_result(str);
 	}
 	else if (atoi(argv[1]) == 6)
 	{
-		if (!(str = ft_strrchr("bonjour", '\0')))
+		if (!(str = ft_strrchr(str2, '\0')))
 			ft_print_result("NULL");
 		else
+		{
 			ft_print_result(str);
+			if (str != str2 + 7)
+				ft_print_result("\nReturn value is false");
+		}
 	}
 	return (0);
 }
