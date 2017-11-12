d666b1d422f4f4e098f875cb1ae1ecb8b1e37a70
Deville Matthew <kemiimek@live.fr>
2017-11-12 22:36:03 +0100

    Add fsanitize to compiling

--- srcs/check_compilation.sh
+++ srcs/check_compilation.sh
@@ -6,7 +6,7 @@
 #    By: jtoty <jtoty@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/01/23 18:26:19 by jtoty             #+#    #+#              #
-#    Updated: 2017/01/23 18:26:20 by jtoty            ###   ########.fr        #
+#    Updated: 2017/11/12 21:27:11 by mdeville         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,7 @@ compilation()
 		rm -f ${PATH_TEST}/user_exe
 	fi
 	printf "$> clang -Wextra -Wall -Werror $1 main.c libft.a -o user_exe\n\n" >> ${PATH_DEEPTHOUGHT}/deepthought
-	clang -Wextra -Wall -Werror ${PATH_LIBFT}/$1 ${PATH_TEST}/tests/$(echo ${part}tions)/$(echo $1 | cut -d . -f 1)/main.c ${PATH_LIBFT}/libft.a 2>>${PATH_DEEPTHOUGHT}/deepthought -o user_exe
+	clang -fsanitize=address -Wextra -Wall -Werror ${PATH_LIBFT}/$1 ${PATH_TEST}/tests/$(echo ${part}tions)/$(echo $1 | cut -d . -f 1)/main.c ${PATH_LIBFT}/libft.a 2>>${PATH_DEEPTHOUGHT}/deepthought -o user_exe
 }
 
 check_compilation()
