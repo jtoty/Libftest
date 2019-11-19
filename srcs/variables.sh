3221588d74fe9b8c86f265b9fa4eef7fe4fe73f1
julien <totyj@free.fr>
2019-11-20 00:09:07 +0100

    refactor: update authorized functions variable for p1, p2 and bonus part

--- srcs/variables.sh
+++ srcs/variables.sh
@@ -16,31 +16,58 @@
 #                 Part1 functions                 #
 ###################################################
 
+Part1_func=(
+	'ft_atoi.c'		\
+	'ft_bzero.c'	\
+	'ft_calloc.c'	\
+	'ft_isalnum.c'	\
+	'ft_isalpha.c'	\
+	'ft_isascii.c'	\
+	'ft_isdigit.c'	\
+	'ft_isprint.c'	\
+	'ft_memccpy.c'	\
+	'ft_memchr.c'	\
+	'ft_memcmp.c'	\
+	'ft_memcpy.c'	\
+	'ft_memmove.c'	\
+	'ft_memset.c'	\
+	'ft_strchr.c' 	\
+	'ft_strdup.c'	\
+	'ft_strlcat.c'	\
+	'ft_strlcpy.c'	\
+	'ft_strlen.c'	\
+	'ft_strncmp.c'	\
+	'ft_strnstr.c'	\
+	'ft_strrchr.c'	\
+	'ft_tolower.c'	\
+	'ft_toupper.c'	\
+)
+
 Part1_func_authorized=(
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
+	'0'	\
+	'0'	\
+	'1'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'1'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
+	'0'	\
 )
 
 Part1_func_activation=(
@@ -70,48 +97,34 @@ Part1_func_activation=(
 	'0'	\
 )
 
-Part1_func=(
-	'ft_atoi.c'		\
-	'ft_bzero.c'	\
-	'ft_calloc.c'	\
-	'ft_isalnum.c'	\
-	'ft_isalpha.c'	\
-	'ft_isascii.c'	\
-	'ft_isdigit.c'	\
-	'ft_isprint.c'	\
-	'ft_memccpy.c'	\
-	'ft_memchr.c'	\
-	'ft_memcmp.c'	\
-	'ft_memcpy.c'	\
-	'ft_memmove.c'	\
-	'ft_memset.c'	\
-	'ft_strchr.c' 	\
-	'ft_strdup.c'	\
-	'ft_strlcat.c'	\
-	'ft_strlcpy.c'	\
-	'ft_strlen.c'	\
-	'ft_strncmp.c'	\
-	'ft_strnstr.c'	\
-	'ft_strrchr.c'	\
-	'ft_tolower.c'	\
-	'ft_toupper.c'	\
-)
-
 ###################################################
 #                 Part2 functions                 #
 ###################################################
 
+Part2_func=(
+	'ft_itoa.c'			\
+	'ft_putchar_fd.c'	\
+	'ft_putendl_fd.c'	\
+	'ft_putnbr_fd.c'	\
+	'ft_putstr_fd.c'	\
+	'ft_split.c'		\
+	'ft_strjoin.c'		\
+	'ft_strmapi.c'		\
+	'ft_strtrim.c'		\
+	'ft_substr.c'		\
+)
+
 Part2_func_authorized=(
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
+	'1'	\
+	'4'	\
+	'4'	\
+	'4'	\
+	'4'	\
+	'3'	\
+	'1'	\
+	'1'	\
+	'1'	\
+	'1'	\
 )
 
 Part2_func_activation=(
@@ -127,33 +140,32 @@ Part2_func_activation=(
 	'0'	\
 )
 
-Part2_func=(
-	'ft_itoa.c'			\
-	'ft_putchar_fd.c'	\
-	'ft_putendl_fd.c'	\
-	'ft_putnbr_fd.c'	\
-	'ft_putstr_fd.c'	\
-	'ft_split.c'		\
-	'ft_strjoin.c'		\
-	'ft_strmapi.c'		\
-	'ft_strtrim.c'		\
-	'ft_substr.c'		\
-)
-
 ###################################################
 #                 Bonus functions                 #
 ###################################################
 
+Bonus_func=(
+	'ft_lstadd_back.c'	\
+	'ft_lstadd_front.c'	\
+	'ft_lstclear.c'		\
+	'ft_lstdelone.c'	\
+	'ft_lstiter.c'		\
+	'ft_lstlast.c'		\
+	'ft_lstmap.c'		\
+	'ft_lstnew.c'		\
+	'ft_lstsize.c'		\
+)
+
 Bonus_func_authorized=(
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
-	'7'	\
+	'0'	\
+	'0'	\
+	'2'	\
+	'2'	\
+	'0'	\
+	'0'	\
+	'3'	\
+	'1'	\
+	'0'	\
 )
 
 Bonus_func_activation=(
@@ -166,18 +178,6 @@ Bonus_func_activation=(
 	'0'	\
 	'0'	\
 	'0'	\
-)
-
-Bonus_func=(
-	'ft_lstadd_back.c'	\
-	'ft_lstadd_front.c'	\
-	'ft_lstclear.c'		\
-	'ft_lstdelone.c'	\
-	'ft_lstiter.c'		\
-	'ft_lstlast.c'		\
-	'ft_lstmap.c'		\
-	'ft_lstnew.c'		\
-	'ft_lstsize.c'		\
 )
 
 ###################################################
