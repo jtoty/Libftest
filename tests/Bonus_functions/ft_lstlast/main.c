7fe3620e75c02565650c393d536ec4d2bb924b6f
julien <totyj@free.fr>
2019-11-21 01:27:15 +0100

    test: create tests for new bonus functions

---
+++ tests/Bonus_functions/ft_lstlast/main.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtoty <jtoty@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/20 16:16:05 by jtoty             #+#    #+#             */
+/*   Updated: 2019/11/20 17:55:49 by jtoty            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include "../../../libft.h"
+#include <string.h>
+
+void	ft_print_result(char const *s)
+{
+	int		len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	write(1, s, len);
+}
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*elem;
+
+	elem = (t_list *)malloc(sizeof(t_list));
+	if (!elem)
+		return (NULL);
+	if (!content)
+	{
+		elem->content = NULL;
+		elem->content_size = 0;
+	}
+	else
+	{
+		if (!(elem->content = malloc(sizeof(*(elem->content)) * content_size)))
+			return (NULL);
+		elem->content = memcpy(elem->content, content, content_size);
+		elem->content_size = content_size;
+	}
+	elem->next = NULL;
+	return (elem);
+}
+
+int main(int argc, const char *argv[])
+{
+	int			arg;
+	t_list		*elem;
+	t_list		*elem2;
+	t_list		*elem3;
+	t_list		*elem4;
+	char		str [] = "lorem";
+	char		str2 [] = "ipsum";
+	char		str3 [] = "dolor";
+	char		str4 [] = "sit";
+
+	elem = ft_lstnew(str, sizeof(str));
+	elem2 = ft_lstnew(str2, sizeof(str2));
+	elem3 = ft_lstnew(str3, sizeof(str3));
+	elem4 = ft_lstnew(str4, sizeof(str4));
+	alarm(5);
+	if (argc == 1 || !elem || !elem2 || !elem3 || !elem4)
+		return (0);
+	elem->next = elem2;
+	elem2->next = elem3;
+	elem3->next = elem4;
+	if ((arg = atoi(argv[1])) == 1)
+	{
+		if (elem4 != ft_lstlast(elem))
+			ft_print_result("Last elem was not returned\n");
+		else
+		{
+			ft_print_result(elem4->content);
+			write(1, "\n", 1);
+		}
+	}
+	else if (arg == 2)
+	{
+		if (elem4 != ft_lstlast(elem4))
+			ft_print_result("Last elem was not returned\n");
+		else
+		{
+			ft_print_result(elem4->content);
+			write(1, "\n", 1);
+		}
+	}
+
+	return (0);
+}
---
+++ tests/Bonus_functions/ft_lstsize/main.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtoty <jtoty@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/20 15:16:05 by jtoty             #+#    #+#             */
+/*   Updated: 2019/11/20 15:55:49 by jtoty            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include "../../../libft.h"
+#include <string.h>
+
+void	ft_print_result2(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_result(int n)
+{
+	if (n >= 0)
+	{
+		if (n >= 10)
+			ft_print_result(n / 10);
+		ft_print_result2(n % 10 + '0');
+	}
+	else
+	{
+		ft_print_result2('-');
+		if (n <= -10)
+			ft_print_result(n / -10);
+		ft_print_result2(n % -10 * -1 + '0');
+	}
+}
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*elem;
+
+	elem = (t_list *)malloc(sizeof(t_list));
+	if (!elem)
+		return (NULL);
+	if (!content)
+	{
+		elem->content = NULL;
+		elem->content_size = 0;
+	}
+	else
+	{
+		if (!(elem->content = malloc(sizeof(*(elem->content)) * content_size)))
+			return (NULL);
+		elem->content = memcpy(elem->content, content, content_size);
+		elem->content_size = content_size;
+	}
+	elem->next = NULL;
+	return (elem);
+}
+
+int main(int argc, const char *argv[])
+{
+	int			arg;
+	t_list		*elem;
+	t_list		*elem2;
+	t_list		*elem3;
+	t_list		*elem4;
+	char		str [] = "lorem";
+	char		str2 [] = "ipsum";
+	char		str3 [] = "dolor";
+	char		str4 [] = "sit";
+
+	elem = ft_lstnew(str, sizeof(str));
+	elem2 = ft_lstnew(str2, sizeof(str2));
+	elem3 = ft_lstnew(str3, sizeof(str3));
+	elem4 = ft_lstnew(str4, sizeof(str4));
+	alarm(5);
+	if (argc == 1 || !elem || !elem2 || !elem3 || !elem4)
+		return (0);
+	elem->next = elem2;
+	elem2->next = elem3;
+	elem3->next = elem4;
+	if ((arg = atoi(argv[1])) == 1)
+	{
+		ft_print_result(ft_lstsize(elem));
+	}
+	else if (arg == 2)
+	{
+		ft_print_result(ft_lstsize(elem4));
+	}
+	else if (arg == 3)
+	{
+		ft_print_result(ft_lstsize(NULL));
+	}
+	return (0);
+}
