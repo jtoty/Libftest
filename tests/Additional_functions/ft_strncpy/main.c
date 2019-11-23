c6fd824df9dab10460e70bb8ff1926f86dfe5b78
julien <totyj@free.fr>
2019-11-23 02:49:24 +0100

    refactor: tests

--- tests/Additional_functions/ft_strncpy/main.c
+++ tests/Additional_functions/ft_strncpy/main.c
@@ -15,56 +15,53 @@
 #include <string.h>
 #include "libft.h"
 
+static void		check_strncat(char *dest, char *src, int n, int len)
+{
+	if (dest != ft_strncpy(dest, src, n))
+		write(1, "dest's adress was not returned\n", 31);
+	write(1, dest, len);
+}
+
 int		main(int argc, const char *argv[])
 {
 	char	*dest;
 	int		arg;
+	int		len;
 
 	alarm(5);
-	if (!(dest = (char *)malloc(sizeof(*dest) * 15)) || argc == 1)
+	len = 15;
+	if (!(dest = (char *)malloc(sizeof(*dest) * len)) || argc == 1)
 		return (0);
 	memset(dest, 0, 15);
 	if ((arg = atoi(argv[1])) == 1)
 	{
 		memset(dest, 's', 13);
-		if (dest != ft_strncpy(dest, "lorem", 6))
-			write(1, "dest's adress was not returned\n", 31);
-		write(1, dest, 15);
+		check_strncpy(dest, "lorem", 6, len);
 	}
 	else if (arg == 2)
 	{
 		memset(dest, 's', 13);
-		if (dest != ft_strncpy(dest, "lorem", 8))
-			write(1, "dest's adress was not returned\n", 31);
-		write(1, dest, 15);
+		check_strncpy(dest, "lorem", 8, len);
 	}
 	else if (arg == 3)
 	{
 		memset(dest, 's', 13);
-		if (dest != ft_strncpy(dest, "", 3))
-			write(1, "dest's adress was not returned\n", 31);
-		write(1, dest, 15);
+		check_strncpy(dest, "", 3, len);
 	}
 	else if (arg == 4)
 	{
 		memset(dest, 's', 13);
-		if (dest != ft_strncpy(dest, "lorem ipsum", 3))
-			write(1, "dest's adress was not returned\n", 31);
-		write(1, dest, 15);
+		check_strncpy(dest, "lorem ipsum", 3, len);
 	}
 	else if (arg == 5)
 	{
 		memset(dest, 's', 13);
-		if (dest != ft_strncpy(dest, "lorem ipsum", 0))
-			write(1, "dest's adress was not returned\n", 31);
-		write(1, dest, 15);
+		check_strncpy(dest, "lorem ipsum", 0, len);
 	}
 	else if (arg == 6)
 	{
 		memset(dest, 's', 5);
-		if (dest != ft_strncpy(dest, "lorem ipsum", 10))
-			write(1, "dest's adress was not returned\n", 31);
-		write(1, dest, 15);
+		check_strncpy(dest, "lorem ipsum", 10, len);
 	}
 	free(dest);
 	return (0);
